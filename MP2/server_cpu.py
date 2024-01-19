d53b6defa316ca6626b5284054185fb374e49efb
lycpaul <ycleepaul@gmail.com>
2024-01-19 19:30:20 +0800

    updated server_cpu

--- MP2/server_cpu.py
+++ MP2/server_cpu.py
@@ -6,7 +6,7 @@ app = Flask(__name__)
 
 @app.route('/', methods=['POST'])
 def start_stress_cpu():
-    subprocess.Popen(['python', 'stress_cpu.py'])
+    subprocess.Popen(['python3', 'stress_cpu.py'])
     return jsonify({'message': 'Stress CPU process started successfully'})
 
 @app.route('/', methods=['GET'])
